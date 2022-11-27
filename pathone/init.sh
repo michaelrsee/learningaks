aec3c0f6c813ecd66445c9d4ea58e185454732f8
michaelrsee <michael.r.see@outlook.com>
2022-11-27 16:36:18 -0500

    successful deployment for p1

--- pathone/init.sh
+++ pathone/init.sh
@@ -37,7 +37,8 @@ az aks update \
 
 export DNS_NAME=$(az network dns zone list -o json --query "[?contains(resourceGroup,'$RESOURCE_GROUP_NAME')].name" -o tsv)
 
-sed -i '' 's+!IMAGE!+'"$ACR_NAME"'/contoso-website+g' kubernetes/deployment.yaml
+echo "Updating yaml files..."
+sed -i '' 's+!IMAGE!+'"$ACR_NAME"'/akspathone+g' kubernetes/deployment.yaml
 sed -i '' 's+!DNS!+'"$DNS_NAME"'+g' kubernetes/ingress.yaml
 
 echo "Installation concluded, copy these values and store them, you'll use them later in this exercise:"
